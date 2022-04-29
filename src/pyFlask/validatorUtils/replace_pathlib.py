675d4bd3f1c6e603299c8d4f3e9c8fc2228ae608
aaronm-2112 <aaron.marroquin96@gmail.com>
2022-04-28 17:46:15 -0700

    URL idea added

--- src/pyFlask/validatorUtils/replace_pathlib.py
+++ src/pyFlask/validatorUtils/replace_pathlib.py
@@ -6,7 +6,22 @@ target_file = '/home/anaconda3/envs/env-validator/lib/python3.7/pathlib.py'
 # read contents from file
 target_file_input = 'replace_pathlib.py'
 
-# 
+# remove the contents of the target file
+with open(target_file, 'w') as f:
+    f.write('')
+
+# place the contents of the source file into the target file
+with open(target_file_input, 'r') as f:
+    with open(target_file, 'a') as f2:
+        for line in f:
+            f2.write(line)
 
 
 
+# use github api to get the pathlib.py file on branch 3.7
+# url = "https://api.github.com/repos/python/cpython/git/trees/3.7?recursive=1"
+
+
+# import requests
+# resp = requests.get(url)
+# print(resp.content)
